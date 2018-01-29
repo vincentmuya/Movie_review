b91e72ec98d7f80d720a66d70eff78b046890917
vincentmuya <vincentmuya13@gmail.com>
2018-01-29 13:13:17 +0300

    added blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,8 @@ def create_app(config_name):
 
     # Initializing flask extensions
     bootstrap.init_app(app)
-
+    
+# Registering the blueprint
 from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
