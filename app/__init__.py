5cc22e09b4cbe8aeb515484718d77ba1cecbc163
vincentmuya <vincentmuya13@gmail.com>
2018-01-31 12:15:50 +0300

    created class user

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
-from flask_sqlalchemy import SQLALchemy
+from flask_sqlalchemy import SQLAlchemy
 
 bootstrap = Bootstrap()
-db = SQLALchemy()
+db = SQLAlchemy()
 
 def create_app(config_name):
 
