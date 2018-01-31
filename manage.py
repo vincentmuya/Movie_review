83c5896a9f469225175ced8d8b33c839b3034a2c
vincentmuya <vincentmuya13@gmail.com>
2018-01-31 15:54:44 +0300

    created class user and class role

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
-from app import create_app
+from app import create_app,db
 from flask_script import Manager,Server
+from app.models import User
 
 # Creating app instance
 app = create_app('development')
@@ -12,6 +13,10 @@ def test():
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+
+@manager.shell
+def make_shell_contex():
+    return dict(app = app,db = db,User = User)
 
 if __name__ == '__main__':
     manager.run()
