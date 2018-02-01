198a2f2da6ce8e1b91f12de41991c73a6a10ddf8
vincentmuya <vincentmuya13@gmail.com>
2018-02-01 04:37:03 +0300

    Authenticated routes

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
@@ -16,6 +17,10 @@ def create_app(config_name):
     # Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
+    login_manager = LoginManager()
+    login_manager.session_protection = 'strong'
+    login_manager.login_view = 'auth.login'
+
 
 # Registering the blueprint
     from .main import main as main_blueprint
