d0a1a698335a1eb09332ce3fbe9a2dc8b89380fc
vincentmuya <vincentmuya13@gmail.com>
2018-02-01 04:41:38 +0300

    imported usermixin and added emaic column

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+
 class Movie:
     '''
     Movie class to define Movie Objects
@@ -45,10 +47,11 @@ class Review:
 
         return response
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+     email = db.Column(db.String(255),unique = True,index = True)
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
     pass_secure =db.Column(db.String(255))
 
